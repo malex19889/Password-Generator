f92729799d67385b23edd9607b700440a7639887
malex19889 <malex19889@yahoo.com>
2020-08-07 15:58:32 -0500

    adding working number only password section and letter arrays

--- Develop/script.js
+++ Develop/script.js
@@ -10,18 +10,22 @@ function writePassword() {
   var uprCaseConf = confirm("Would you like any uppercase letter in your password?");
   var specCharConf = confirm("Would you like any special characters in your password?");
   var numbers = [1,2,3,4,5,6,7,8,9,0,];
+  var lwrCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  var uprCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
-
+  var password=[]
   for (var i = 0; i < pwdLength; i++) {
-    var password=[" "]
-    var password[i] = numbers[Math.floor(Math.random() * numbers.length)];
-    console.log(password)
+    if (numConf = true) {
+      password[i] = numbers[Math.floor(Math.random() * numbers.length)];
   }
+    }
+    
+    
   
 
 
   var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  passwordText.value = password.join("");
 
 }
 
