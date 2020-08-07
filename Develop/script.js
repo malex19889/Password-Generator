d235df9fa2915c35ba55dc8993f0ad2ad60890e7
malex19889 <malex19889@yahoo.com>
2020-08-07 16:40:48 -0500

    adding working num and lower case generator code line 26-30

--- Develop/script.js
+++ Develop/script.js
@@ -9,14 +9,24 @@ function writePassword() {
   var lwrCaseConf = confirm("Would you like lower case letters in your password?");
   var uprCaseConf = confirm("Would you like any uppercase letter in your password?");
   var specCharConf = confirm("Would you like any special characters in your password?");
+  
   var numbers = [1,2,3,4,5,6,7,8,9,0,];
   var lwrCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
   var uprCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-
+  var specChar = []
+  var comboArr = []
+  
   var password=[]
+
   for (var i = 0; i < pwdLength; i++) {
-    if (numConf = true) {
+    if ((numConf = true) || (lwrCaseConf = false )|| (uprCaseConf = false) || (specCharConf =false)) {
+
       password[i] = numbers[Math.floor(Math.random() * numbers.length)];
+
+  }if ((numConf = true) || (lwrCaseConf = true )|| (uprCaseConf = false) || (specCharConf =false)) {
+    comboArr = numbers.concat(lwrCase)
+    password[i] = comboArr[Math.floor(Math.random() * comboArr.length)]
+
   }
     }
     
