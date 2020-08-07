c157a95b3f3afdaf547532ad1aa713df81b6c0a5
malex19889 <malex19889@yahoo.com>
2020-08-07 15:25:33 -0500

    adding my starting code changes

--- Develop/script.js
+++ Develop/script.js
@@ -3,9 +3,24 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // var password = generatePassword();
+  var pwdLength = prompt("Enter desired password length. Must between 8-128");
+  var numConf = confirm("Would you like numbers in your password?");
+  var lwrCaseConf = confirm("Would you like lower case letters in your password?");
+  var uprCaseConf = confirm("Would you like any uppercase letter in your password?");
+  var specCharConf = confirm("Would you like any special characters in your password?");
+  var numbers = [1,2,3,4,5,6,7,8,9,0,];
 
+
+  for (var i = 0; i < pwdLength; i++) {
+    var password=[" "]
+    var password[i] = numbers[Math.floor(Math.random() * numbers.length)];
+    console.log(password)
+  }
+  
+
+
+  var passwordText = document.querySelector("#password");
   passwordText.value = password;
 
 }
