70bcee110639e97f0d44c11476e334cb054104b1
malex19889 <malex19889@yahoo.com>
2020-08-08 22:02:55 -0500

    reworked code from multiple long  arguments to if statements

--- Develop/script.js
+++ Develop/script.js
@@ -3,45 +3,55 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  // var password = generatePassword();
-  var pwdLength = prompt("Enter desired password length. Must between 8-128");
-  var numConf = confirm("Would you like numbers in your password?");
-  var lwrCaseConf = confirm("Would you like lower case letters in your password?");
-  var uprCaseConf = confirm("Would you like any uppercase letter in your password?");
-  var specCharConf = confirm("Would you like any special characters in your password?");
-  
+//  character arrays
   var numbers = [1,2,3,4,5,6,7,8,9,0,];
   var lwrCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
   var uprCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
   var specChar = [ " ","!","#","$","%","&","'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~",]
-  var comboArr = []
   
+  var comboArr = [""]
   var password=[]
-
-  for (var i = 0; i < pwdLength; i++) {
-    if ((numConf = true) || (lwrCaseConf = false )|| (uprCaseConf = false) || (specCharConf =false)) {
-
-      password[i] = numbers[Math.floor(Math.random() * numbers.length)];
-
-  }if ((numConf = true) || (lwrCaseConf = true )|| (uprCaseConf = false) || (specCharConf =false)) {
-    comboArr = numbers.concat(lwrCase)
-    password[i] = comboArr[Math.floor(Math.random() * comboArr.length)]
-
-  }if ((numConf = true) || (lwrCaseConf = true )|| (uprCaseConf = true) || (specCharConf =false)) {
-    comboArr = numbers.concat(lwrCase, uprCase)
-    password[i] = comboArr[Math.floor(Math.random() * comboArr.length)]
-
-  }if ((numConf = true) || (lwrCaseConf = true )|| (uprCaseConf = true) || (specCharConf =true)) {
-    comboArr = numbers.concat(lwrCase, uprCase, specChar)
-    password[i] = comboArr[Math.floor(Math.random() * comboArr.length)]
-
+ 
+  var pwdLength = prompt("Enter desired password length. Must between 8-128");
+  if ((pwdLength <8 || pwdLength >128)) {
+    alert("unable to complete your request");
+    return;
   }
-    }
-    
-    
   
+  var numConf = confirm("Would you like numbers in your password?");
+  if (numConf === true) {
+    comboArr= comboArr.concat(numbers);
+  }
+  if (numConf === false) {
+    comboArr= comboArr;
+  }
+  var lwrCaseConf = confirm("Would you like lower case letters in your password?");
+  if (lwrCaseConf === true) {
+    comboArr= comboArr.concat(lwrCase)
+  }
+  if (lwrCaseConf === false) {
+    comboArr= comboArr;
+  }
+  var uprCaseConf = confirm("Would you like any uppercase letter in your password?");
+  if (uprCaseConf === true) {
+      comboArr= comboArr.concat(uprCase)
+    }
+  if (uprCaseConf === false) {
+      comboArr= comboArr;
+    }
+  var specCharConf = confirm("Would you like any special characters in your password?");
+  if (specCharConf === true) {
+      comboArr= comboArr.concat(specChar)
+    }
+  if (specCharConf === false) {
+      comboArr= comboArr;
+    }
 
+for (var i = 0; i < pwdLength; i++) {
 
+    password[i] = comboArr[Math.floor(Math.random() * comboArr.length)];
+
+ } 
   var passwordText = document.querySelector("#password");
   passwordText.value = password.join("");
 
@@ -49,3 +59,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
